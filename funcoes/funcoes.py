6a6be2bbae585583abcbf67333c7309450f36311
Rafael <ribeirorafaelmatheus@gmail.com>
2020-08-29 12:29:20 -0300

    Indice atualizado e funcões

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -73,3 +73,77 @@ def trocaPU(l):
     aux_2 = l[0]
     l[0] = aux_1
     l[-1] = aux_2
+
+############################################Exercício da Semana 5#############################################################################
+####################################################################################################################################################
+
+### Olhar o Notebook da semana 5#################
+
+
+def problema_3_2():
+    #a)
+    idade = eval(input("Digite sua Idade"))
+    if idade > 62:
+        print("Você pode obter benefícios de pensão")
+    #b)
+    
+    jogadores = ['Musial', 'Aaraon', 'Williams', 'Gehrig', 'Ruth']
+    nome = input("Digite um nome")
+    if nome in jogadores: ()
+        print("Um dos 5 maiores jogadores de beisebol de todos os tempos!")
+    
+    golpes = eval(input("O valor do golpe"))
+    defesas = eval(input("O valor da defesa"))
+    if golpes > 10 and defesas == 0:
+        print("Você está morto")
+    
+    norte = True
+    sul = False
+    leste = False
+    oeste = True
+    if norte or sul or leste or oeste == True:
+        print('Posso escapar.')
+
+
+# 3.3)
+def problema_3_3():
+    #a)
+    ano = eval(input("Digite um ano"))
+    if ano % 4 == 0:
+        print('Pode ser um ano bissexto.')
+    else:
+        print('Nãe é um ano bissexto.')
+
+    #b) 
+    print("Ós números da loteria é 1 número inteiros de 0 a 100\n\n--------------------------------\n\n")
+    bilhete = eval(input("Digite os numeros do bilhete da seguinte forma:\n 15"))
+    np.random.seed(10)
+    loteria = np.random.randint(0,100,1)
+    if bilhete == loteria:
+        print('Você ganhou!')
+    else:
+        print('Melhor sorte da próxima vez...')
+
+def problema_3_4():
+    usuários = ['joe', 'sue', 'hani', 'sophie']
+    id = input('Login: ')
+    if id in usuários:
+        print('Você entrou!')
+    else:
+        print('Usuário desconhecido. ')
+    
+    print('Fim.')
+
+def meuIMC(peso, altura):
+  'exibe relatório do IMC '
+  imc = peso / altura**2
+  if imc < 18.5:
+    print('Abaixo do peso')
+  elif imc < 25:
+    print('Normal')
+  else:                       # pode ser elif imc >= 25
+    print('Sobbrepeso')
+
+
+
+
